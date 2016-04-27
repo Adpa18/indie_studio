699c4b05c6b552ec5dff86759b40d82c35033863
gaspar_q <quentin.gasparotto@epitech.eu>
2016-04-27 19:51:37 +0200

    Debut de test serialisation, je met a jour la version pour pouvoir tester

---
+++ Test/Serialize.cpp
@@ -0,0 +1,31 @@
+//
+// Created by gaspar_q on 4/27/16.
+//
+
+#include <irrlicht.h>
+#include <AGameObject.hpp>
+#include <Player.hpp>
+
+int main()
+{
+    EventGame   eventGame;
+    std::map<ACharacter::ACTION, irr::EKEY_CODE> keycodes;
+    AGameObject *object = new Player("Richard", irr::core::vector3df(0, 0, 0), "./media/pikachu", 42, eventGame, keycodes);
+    irr::IrrlichtDevice *device = IrrlichtController::getDevice();
+    irr::scene::ISceneManager *manager = IrrlichtController::getSceneManager();
+    irr::video::IVideoDriver *driver = IrrlichtController::getDriver();
+
+    while (device->run())
+    {
+        if (device->isWindowActive())
+        {
+            driver->beginScene(true, true, irr::video::SColor(255,200,200,200));
+            manager->drawAll();
+            driver->endScene();
+        }
+        else
+            device->yield();
+    }
+
+    return (0);
+}
