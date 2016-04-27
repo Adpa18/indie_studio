5200c53596aa2d9b9be462c31dae7fd95087cecc
Victor <victor.gouet@epitech.eu>
2016-04-27 18:31:34 +0200

    Wall and BomberMap created

---
+++ src/BomberMap.cpp
@@ -0,0 +1,21 @@
+//
+// BomberMap.cpp for MAP in /home/gouet_v/Rendu/semester4/CPP/cpp_indie_studio
+// 
+// Made by Victor Gouet
+// Login   <gouet_v@epitech.net>
+// 
+// Started on  Wed Apr 27 18:14:09 2016 Victor Gouet
+// Last update Wed Apr 27 18:18:04 2016 Victor Gouet
+//
+
+#include "../include/BomberMap.hpp"
+
+BomberMap::BomberMap(std::string const &)
+{
+  
+}
+
+BomberMap::BomberMap(int width, int height)
+{
+  
+}
---
+++ src/Wall.cpp
@@ -0,0 +1,27 @@
+//
+// Wall.cpp for WALL in /home/gouet_v/Rendu/semester4/CPP/cpp_indie_studio
+// 
+// Made by Victor Gouet
+// Login   <gouet_v@epitech.net>
+// 
+// Started on  Wed Apr 27 18:19:48 2016 Victor Gouet
+// Last update Wed Apr 27 18:30:10 2016 Victor Gouet
+//
+
+#include "../include/Wall.hpp"
+
+Wall::Wall(irr::core::vector3df const &pos, State state)
+  : AGameObject(pos, ""), _state(state)
+{
+  
+}
+
+Wall::~Wall()
+{
+  
+}
+
+Wall::State			Wall::getState() const
+{
+  return (_state);
+}
