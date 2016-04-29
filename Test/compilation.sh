7ee4c95d5526af42f17a57ebdc48586163a1e55e
Victor <victor.gouet@epitech.eu>
2016-04-29 12:00:09 +0200

    compilation

--- Test/compilation.sh
+++ Test/compilation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ *.cpp ../src/Collider.cpp ../src/ACharacter.cpp ../src/AGameObject.cpp ../src/EventGame.cpp ../src/IrrlichtController.cpp ../src/Player.cpp -I ../include -I ../Irrlicht/irrlicht-1.8.3/include -I /usr/X11R6/include -L ../Irrlicht/irrlicht-1.8.3/lib/Linux -lIrrlicht -L /usr/X11R6/lib64 -lGL -lXxf86vm -lXext -lX11 -lXcursor -std=c++11
+g++ *.cpp ../src/Collider.cpp ../src/ACharacter.cpp ../src/AGameObject.cpp ../src/EventGame.cpp ../src/IrrlichtController.cpp ../src/Player.cpp ../src/AController.cpp ../src/MotionController.cpp ../src/KeysController.cpp -I ../include -I ../Irrlicht/irrlicht-1.8.3/include -I /usr/X11R6/include -L ../Irrlicht/irrlicht-1.8.3/lib/Linux -lIrrlicht -L /usr/X11R6/lib64 -lGL -lXxf86vm -lXext -lX11 -lXcursor -std=c++11
