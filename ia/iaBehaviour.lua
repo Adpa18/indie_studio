27a06d96accdca60d443a87de4a4b50dc45505fc
gaspar_q <quentin.gasparotto@epitech.eu>
2016-05-06 18:59:23 +0200

    lib lua push avec debut de class lua handler qui permet de recup les fonctions du fichier lua et de les appeler avec n'importe quels arguments, les erreurs ne sont pas recuperees

---
+++ ia/iaBehaviour.lua
@@ -0,0 +1,15 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: gaspar_q
+-- Date: 5/6/16
+-- Time: 4:18 PM
+-- To change this template use File | Settings | File Templates.
+--
+
+function computeIA(x, y)
+    return x + y;
+end
+
+function data()
+    return 2;
+end
---
+++ ia/test.cpp
@@ -0,0 +1,27 @@
+//
+// Created by gaspar_q on 5/6/16.
+//
+
+#include <iostream>
+#include "LuaHandler.hpp"
+
+int main()
+{
+    Lua::LuaHandler  handler("./iaBehaviour.lua");
+
+    std::cout << handler["computeIA"](2, 3) << std::endl;
+    /*lua_State   *state;
+
+    state = luaL_newstate();
+    luaL_openlibs(state);
+    luaL_dofile(state, "./iaBehaviour.lua");
+    lua_getglobal(state, "computeIA");
+    lua_pushnumber(state, 4);
+    lua_pushnumber(state, 6);
+    lua_call(state, 2, 2);
+    std::cout << lua_tointeger(state, -1) << std::endl;
+    lua_pop(state, 1);
+    std::cout << lua_tointeger(state, -1) << std::endl;
+    lua_close(state);*/
+    return (0);
+}
