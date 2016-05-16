f0049a17170cd8328661b54deeec99b6733ac7ea
Adrien WERY <adrien.wery@epitech.eu>
2016-05-16 18:02:16 +0200

    video OK

--- Video/Video.cpp
+++ Video/Video.cpp
@@ -11,6 +11,9 @@
 #include "Video.hpp"
 #include "../include/IrrlichtController.hpp"
 
+const std::string     Video::default_path_video = "../intro/video/";
+const std::string     Video::default_path_sound = "../intro/sound/";
+
 Video::Video(const std::string path)
 {
   DIR	*rep;
@@ -23,7 +26,7 @@ Video::Video(const std::string path)
       while ((ent = readdir(rep)) != NULL)
 	{
 	  if (std::string(ent->d_name) != "." && std::string(ent->d_name) != "..")
-	    this->files.push_back(path + "/" + std::string(ent->d_name));
+	    this->files.push_back(default_path_video + std::string(ent->d_name));
 	}
       closedir(rep);
       std::sort(this->files.begin(), this->files.end());
@@ -36,40 +39,31 @@ Video::~Video()
 
 void	Video::start()
 {
-  irrklang::ISoundEngine *engine = irrklang::createIrrKlangDevice();
-  if (!engine)
-    return;
-  IrrlichtController::width = 1920;
-  IrrlichtController::height = 1080;
-  irr::IrrlichtDevice* device = IrrlichtController::getDevice();
-  irr::video::IVideoDriver* driver = device->getVideoDriver();
-  std::vector<irr::video::ITexture *> frames;
-  for (std::vector<std::string>::const_iterator it = this->files.begin(); it != this->files.end(); ++it)
-    frames.push_back(driver->getTexture((*it).data()));
-  irr::scene::ISceneManager *sceneManager = device->getSceneManager();
-  irr::core::position2d<irr::s32> position0;
-  position0.X = 0;
-  position0.Y = 0;
-  //std::vector<std::string>::const_iterator it = this->files.begin();
-  clock_t	timerFrame;
-  //irr::video::ITexture	*frame;
-  int	i = 0;
-  std::vector<irr::video::ITexture *>::iterator it = frames.begin();
-  while (device->run() && it != frames.end())
+    IrrlichtController::width = 1270;
+    IrrlichtController::height = 720;
+    irr::IrrlichtDevice* device = IrrlichtController::getDevice(false);
+
+    irrklang::ISoundEngine *engine = irrklang::createIrrKlangDevice();
+    if (!engine)
+        return;
+    irr::video::IVideoDriver* driver = device->getVideoDriver();
+    size_t  i = 0;
+    clock_t	timerFrame;
+    while (device->run() && i < this->files.size())
     {
-      timerFrame = clock();
-      //frame = driver->getTexture((*it).data());
-      driver->beginScene(true, true, irr::video::SColor(0,0,0,0));
-      driver->draw2DImage(*it, position0);
-      driver->endScene();
-      if (i == 32)
-	engine->play2D("../intro/intro.wav", true);
-      timerFrame = clock() - timerFrame;
-      if (static_cast<float>(timerFrame) / CLOCKS_PER_SEC < 1.0 / 23.975)  
-	usleep(1000000.0 / 23.975 - static_cast<float>(timerFrame) / CLOCKS_PER_SEC * 1000000.0);
-      ++it;
-      ++i;
+        if (i == 24)
+            engine->play2D((default_path_sound + "intro.wav").c_str(), true);
+        timerFrame = clock();
+        irr::video::ITexture *tex = driver->getTexture((this->files[i]).c_str());
+        driver->beginScene(true, true, irr::video::SColor(0,0,0,0));
+        driver->draw2DImage(tex, irr::core::position2d<irr::s32>(0, 0));
+        driver->endScene();
+        driver->removeTexture(tex);
+        timerFrame = clock() - timerFrame;
+        if (static_cast<float>(timerFrame) / CLOCKS_PER_SEC < 1.0 / 23.975)
+            usleep(1000000.0 / 23.975 - static_cast<float>(timerFrame) / CLOCKS_PER_SEC * 1000000.0);
+        ++i;
     }
-  engine->drop();
+    engine->drop();
   device->drop();
 }
