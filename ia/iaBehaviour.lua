268272404df5e9c10ae089dcfad83400e4dec6a3
gaspar_q <quentin.gasparotto@epitech.eu>
2016-05-25 12:32:50 +0200

    le code de l'ia est commenté pour s'y retrouver plus facilement

--- ia/iaBehaviour.lua
+++ ia/iaBehaviour.lua
@@ -6,10 +6,9 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-function dirTab()
-    return {[RIGHT] = Vector2.creat(1, 0), [LEFT] = Vector2.creat(-1, 0), [UP] = Vector2.creat(0, 1), [DOWN] = Vector2.creat(0, -1)};
-end
-
+--[[
+--Function like 'pairs' function for 'for in' in reverse loop
+ ]]
 function ripairs(t)
     local max = 1
     while t[max] ~= nil do
@@ -27,22 +26,35 @@ function ripairs(t)
     return ripairs_it, t, max
 end
 
+--[[
+--Return a table of 4 directions with the key corresponding to the macro
+ ]]
+function dirTab()
+    return {[RIGHT] = Vector2.creat(1, 0), [LEFT] = Vector2.creat(-1, 0), [UP] = Vector2.creat(0, 1), [DOWN] = Vector2.creat(0, -1)};
+end
+
+--[[
+--Return the direction corresponding to the code send in parameter
+ ]]
 function getDirFromCode(code)
     return (dirTab()[code]);
 end
 
-posSeen = {};
-
+--[[
+--Check if the position is in the map
+ ]]
 function isInMap(pos)
     if (pos:getX() < 0 or pos:getX() >= MapW or
-        pos:getY() < 0 or pos:getY() >= MapH) then
+            pos:getY() < 0 or pos:getY() >= MapH) then
         return (false);
     end
     return (true);
 end
 
+--[[
+--Check if the player can move to pos
+ ]]
 function canMoveOnPos(pos)
---    local tocheck = bomberMap:objsAtPos(pos:getX(), pos:getY());
     local tocheck = bomberMap:getDangerAtPos(pos:getX(), pos:getY());
 
     if (tocheck ~= NONE  or
@@ -52,147 +64,63 @@ function canMoveOnPos(pos)
     return true;
 end
 
+--[[
+--Get all possibles position that the player is allowed to go
+ ]]
 function getPossiblePos(pos)
     local tocheck = dirTab();
     local possib = {};
     local i = 0;
 
---    print("from ("..pos:getX()..", "..pos:getY()..")");
     for k, dir in pairs(tocheck) do
         local gonnasee = pos:add(dir);
         if (canMoveOnPos(gonnasee)) then
             i = i + 1;
             possib[i] = k;
---            print("move "..k.." is possible");
---        else
---            print("move "..k.." is impossible");
---            print("position ("..gonnasee:getX()..", "..gonnasee:getY()..") is unreachable");
         end
     end
---    print(i.." moves possible");
     return possib, i;
 end
 
+--[[
+--Used for positions that are already saw
+ ]]
+posSeen = {};
+
+--[[
+--Find the first impasse in the map
+ ]]
 function findFirstImpasse(pos)
     local directions = dirTab();
 
     posSeen[pos:getX() + pos:getY() * MapW] = true;
---    print("We are on ("..pos:getX()..", "..pos:getY()..")");
     for _, dir in pairs(directions) do
         local togo = pos:add(dir);
 
---        print("pos("..pos:getX()..", "..pos:getY()..") + dir("..dir:getX()..", "..dir:getY()..") = togo("..togo:getX()..", "..togo:getY()..")");
         if (posSeen[togo:getX() + MapW * togo:getY()] == nil and canMoveOnPos(togo)) then
---            local tolook = bomberMap:objsAtPos(xloop, yloop);
-
---            print("We are checking ("..xloop..", "..yloop..")");
---            print("    this case has "..tolook:size().." type");
---            if (tolook:size() == 0) then
---                print("from ("..pos:getX()..", "..pos:getY()..") case("..xloop..", "..yloop..") is empty");
---                local togo = Vector2.creat(xloop, yloop);
---                print("to go: ("..togo:getX()..", "..togo:getY()..")");
---                togo:setX(xloop);
---                togo:setY(yloop);
             return (findFirstImpasse(togo));
         end
     end
     return (pos);
 end
 
---[[function isCraignos(pos)
-    local i = 0;
-    local posToCheck = dirTab();
-
---    print("check pos: ("..pos:getX()..", "..pos:getY()..")");
-    while (i <= 3 and (posToCheck[1] ~= nil or posToCheck[2] ~= nil or posToCheck[3] ~= nil or posToCheck[4] ~= nil)) do
---        print("for i = "..i);
-        for k, dir in ripairs(posToCheck) do
-            local newpos = pos:add(dir:mul(i));
-            local tolook = bomberMap:objsAtPos(newpos:getX(), newpos:getY());
-            --            print("move: "..k);
---            if (posToCheck[j] ~= nil) then
-
---                                print("checking: ("..newpos:getX()..", "..newpos:getY()..")");
---                                print("    result of ("..dir:getX()..", "..dir:getY()..") * "..i.." + ("..pos:getX()..", "..pos:getY()..")");
-
-            if (tolook:hasType(BOMB) or tolook:hasType(BOOM)) then
---                                            print("is craignos");
-                return (true);
-            elseif (canMoveOnPos(newpos) == false) then
---                    print("----removing "..k.."----");
-                table.remove(posToCheck, k);
-            end
---            end
-        end
-        i = i + 1;
-    end
---    print("is not craignos");
-    return (false);
-end]]
-
---[[function getUncraignosMove(possiblePos, pos)
-    local possib = {};
-    local i = 0;
-
-    for _, poscode in pairs(possiblePos) do
---        print("code: "..poscode);
-        local gonnasee = pos:add(getDirFromCode(poscode));
---        if (canMoveOnPos(gonnasee)) then
---            print("can move on ("..gonnasee:getX()..", "..gonnasee:getY()..")");
---        else
---            print("can not move on ("..gonnasee:getX()..", "..gonnasee:getY()..")");
---        end
-        if (canMoveOnPos(gonnasee)]]--[[ and isCraignos(gonnasee) == false]]--[[) then
---            print("We found a move: "..poscode);
-            i = i + 1;
-            possib[i] = poscode;
---        else
---            print("Move "..poscode.." not valid");
-        end
-    end
-    return possib, i;
-end]]
-
+--[[
+--Choose if the IA will move, drop a bomb or idle
+ ]]
 function waitBombOrMove(iaplayer)
     local possibMove, nbPossib = getPossiblePos(iaplayer:getPos());
---    local uncmov, nbmov = getUncraignosMove(possibMove, pos);
 
---    print("wait bomb or move from: ("..pos:getX()..", "..pos:getY()..")");
---    print("focus = ("..focus:getX()..", "..focus:getY()..")");
     if (iaplayer:getPos():equal(iaplayer:getFocus())) then
---        print("-----------------it is equal-----------------");
---        if (isCraignos(pos)) then
---            print("-----------------but craignos-----------------");
---            posSeen = {};
---            if (nbPossib > 0) then
---                return possibMove[math.random(1, nbPossib)], findFirstImpasse(pos);
---            end
---            return IDLE, findFirstImpasse(pos);
---        end
---        print("-----------------and not craignos-----------------");
         if (nbPossib == 0) then
---            print("idle");
             return IDLE;
         end
-        --bomb
---        print("bomb");
         posSeen = {};
-        --todo simulate bomb drop
         local nextFocus = iaplayer:bombDropSimul();
         if (nextFocus == nil) then
             return (IDLE);
         end
         return DROPBOMB, nextFocus;
     end
---    print("move");
-    --find impasse and move
-    --[[if (nbmov > 0) then
---        for d=1,nbmov do
---            print("index: "..d);
---            print("can move to: "..uncmov[d]);
---        end
-        return uncmov[math.random(1, nbmov)];
-    else]]
     if (nbPossib > 0) then
         return possibMove[math.random(1, nbPossib)];
     end
@@ -200,30 +128,26 @@ function waitBombOrMove(iaplayer)
 end
 
 --todo implement the three behaviours
---todo ajouter la simulation du possage de bombes
+--[[
+--Function for an IA easy behaviour
+ ]]
 function easyBehaviour(iaplayer)
     local fimp;
     local action;
     local focusPos = iaplayer:getFocus();
     local iaPos = iaplayer:getPos();
 
---    bomberMap = bommap;
     if (focusPos:getX() == -1 and focusPos:getY() == -1) then
         posSeen = {};
         fimp = findFirstImpasse(iaPos);
         action = math.random(LEFT, DOWN);
---        print("first impasse = ("..fimp:getX()..", "..fimp:getY()..")");
     else
---        action = IDLE;
         action, fimp = waitBombOrMove(iaplayer);
     end
 
     if (fimp ~= nil) then
         iaplayer:setFocus(fimp);
     end
-
---    bomberMap:getDangerAtPos(iaPos:getX(), iaPos:getY());
---    print("focus = ("..focusPos:getX()..", "..focusPos:getY()..")");
     return (action);
 end
 
@@ -382,8 +306,8 @@ function getObjectif(iaPos)
   return choice;
 end
 
-function mediumBehaviour(iaPos, focusPos)
-  return getObjectif(iaPos);
+function mediumBehaviour(iaplayer)
+  return getObjectif(iaplayer:getPos());
 end
 
 function hardBehaviour(iaPos, focusPos)
